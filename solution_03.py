5432b19300dd4b3ac0982238751ba2f3f3dd5d1e
Stephen Johnson <stephen.c.johnson@gmail.com>
2021-12-03 10:47:18 -0500

    remove some of the copy pastage

--- solution_03.py
+++ solution_03.py
@@ -1,7 +1,8 @@
 import numpy as np
+import operator
 
 
-def o2_filter(consider, index):
+def xo2_filter(consider, index, comparator):
     if len(consider) == 1:
         return consider
 
@@ -13,38 +14,15 @@ def o2_filter(consider, index):
     values = "".join([str(int(i)) for i in np.round(values/length + .001)])
     new_consider = []
     for c in consider:
-        if values[index] == c[index]:
+        if comparator(values[index], c[index]):
             new_consider.append(c)
-    return o2_filter(new_consider, index+1)
+    return xo2_filter(new_consider, index+1, comparator)
 
 
-def o2_rating(file_name):
+def xo2_rating(file_name, comparator):
     consider = [ins.strip() for ins in open(file_name)]
     index = 0
-    return int(o2_filter(consider, index)[0], 2)
-
-
-def co2_filter(consider, index):
-    if len(consider) == 1:
-        return consider
-
-    values = np.zeros(len(consider[1]))
-    length = 0
-    for ins in consider:
-        values += np.array([int(c) for c in ins.strip()])
-        length += 1
-    values = "".join([str(int(i)) for i in np.round(values/length + .001)])
-    new_consider = []
-    for c in consider:
-        if values[index] != c[index]:
-            new_consider.append(c)
-    return co2_filter(new_consider, index+1)
-
-
-def co2_rating(file_name):
-    consider = [ins.strip() for ins in open(file_name)]
-    index = 0
-    return int(co2_filter(consider, index)[0], 2)
+    return int(xo2_filter(consider, index, comparator)[0], 2)
 
 
 def part1(file_name, string_length):
@@ -63,7 +41,8 @@ def part1(file_name, string_length):
 if __name__ == "__main__":
     print("test part 1: ", part1("test_03.txt", 5))
     print("part 1: ", part1("input_03.txt", 12))  # 2498354
-    print("test part 2: ", o2_rating("test_03.txt")*co2_rating("test_03.txt"))
+    print("test part 2: ", xo2_rating("test_03.txt", operator.eq)
+          * xo2_rating("test_03.txt", operator.ne))
 
-    print("part 2: ", o2_rating("input_03.txt")
-          * co2_rating("input_03.txt"))  # 3277956
+    print("part 2: ", xo2_rating("input_03.txt", operator.eq)
+          * xo2_rating("input_03.txt", operator.ne))  # 3277956
