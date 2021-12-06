93e39ff7c993ef4daf004c5b305131bd382d7dc6
Stephen Johnson <stephen.c.johnson@gmail.com>
2021-12-06 08:24:16 -0500

    remove old comment

--- solution_05.py
+++ solution_05.py
@@ -19,15 +19,14 @@ def part1(input_file):
     for l in lines:
         rr, cc = line(l[0], l[1], l[2], l[3])
 
-
         if l[0] != l[2] and l[1] != l[3]:
             image2[rr, cc] += 1
         else:
             image1[rr, cc] += 1
             image2[rr, cc] += 1
-    return (np.sum(image1 > 1), np.sum(image2 >1))
+    return (np.sum(image1 > 1), np.sum(image2 > 1))
 
 
 if __name__ == "__main__":
     print("test solution: ", part1("test_05.txt"))
-    print("solution: ", part1("input_05.txt")) # 15538 too high
+    print("solution: ", part1("input_05.txt"))
